d79e29b5d71d40fd6dfb81e75eda93a24afc51db
nikaba24-meet <babiyn34@gmail.com>
2024-07-22 18:22:15 +0300

    labbbbbbbbbbbbbbbbbbb

--- Auth lab/auth.py
+++ Auth lab/auth.py
@@ -11,12 +11,12 @@ firebaseConfig = {
   "storageBucket" : "auth-lab-b9e1d.appspot.com",
   "messagingSenderId": "746442603326",
   "appId": "1:746442603326:web:8503248029581def9d0a6f",
-  "databaseURL": ""
+  "databaseURL": "https://auth-lab-b9e1d-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
-
+db =firebase.database()
 
 app = Flask(__name__, template_folder='templates', static_folder='static')
 app.config['SECRET_KEY'] = 'really-secret-key'
@@ -27,10 +27,16 @@ def main():
   if request.method == 'POST': 
     email = request.form['email']
     password = request.form['password']
+    fullName = request.form['fullName']
+    username = request.form['username']
+
     try:
+      user = {'fullName':fullName,
+     'username':username, 
+     'email': email}
 
       session['user'] = auth.create_user_with_email_and_password(email, password)
-      session['quotes'] = []
+      db.child('Users').child(session['user']['localId']).set(user)
 
       return redirect('/home')
 
@@ -48,7 +54,7 @@ def signin():
         email = request.form['email']
         password = request.form['password']
         session['user'] = auth.sign_in_with_email_and_password(email, password)
-        session['quotes'] = []
+        
         return redirect('/home')
     except Exception as e:
           print(e)
@@ -60,10 +66,11 @@ def signin():
 def home():
   try:
     if request.method == 'POST':
-     quote = request.form['quote']
-     session['quotes'].append(quote)
-     session.modified = True
-     return render_template('thanks.html',)
+      quote = request.form['quote']
+      who = request.form['who']
+      quote = {'quote':quote, 'who': who, 'uid': session['user']['localId']}
+      allQuotes = db.child('Quotes').push(quote)
+      return render_template('thanks.html')
     else:
       if 'user' not in session:
         return redirect('/signin')
@@ -80,8 +87,7 @@ def thanks():
 def display():
   if 'user' not in session:
         return redirect('/signin') 
-  print(session['quotes'])
-  allQuotes = session['quotes']
+  allQuotes = db.child('Quotes').get().val()
   return render_template('display.html', allQuotes = allQuotes)
 
 @app.route('/signout')
